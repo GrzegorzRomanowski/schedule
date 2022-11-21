d910ddcd6e3eecdc267a0cbef5c66afd56741abe
Grzegorz Romanowski <118511601+GrzegorzRomanowski@users.noreply.github.com>
2022-11-21 20:31:20 +0100

    Add files via upload

---
+++ Classes.py
@@ -0,0 +1,90 @@
+import os, pickle
+script_dir = os.path.dirname(__file__)
+
+EmployeeList = []
+PoolList = []
+
+class LifeGuard:
+    def __init__(self, name, surname, PESEL, hourlyRate):
+        self.name = name
+        self.surname = surname
+        self.PESEL = PESEL
+        self.hourlyRate = hourlyRate
+        self.preferredPool = None
+        self.availability = {}
+        self.schedule = {}
+        self.addLifeGuardToEmployeeList()
+
+    def addLifeGuardToEmployeeList(self):
+        EmployeeList.append(self)       #.PESEL
+
+    def __del__(self):
+        EmployeeList.remove(self)       #.PESEL
+
+    def showLifeGuardInfo(self):
+        return {"Imię" : self.name,
+                "Nazwisko" : self.surname,
+                "PESEL" : self.PESEL,
+                "Stawka godzinowa" : self.hourlyRate,
+                "Preferowany basen" : self.preferredPool}
+
+    def setAvailability(self):
+        v = 1
+        while v <= 7:      #31
+            x = input(str(self.name) + "- Podaj dyspozycyjność w dniu " + str(v) + ". \n0-wolne, 1-pierwsza zmiana, 2-druga zmiana, 3-cały dzień:  ")
+            try:
+                if int(x) in [0, 1, 2, 3]:
+                    self.availability[int(v)] = int(x)
+                    v += 1
+                else:
+                    print("Zły typ danych")
+            except:
+                print("Zły typ danych")
+        fh = open(script_dir + "\\data\\" + str(self.PESEL) + ".dat", "wb")
+        pickle.dump(self.availability, fh)
+        fh.close
+
+
+class Pool:
+    def __init__(self, name, amountOfLifeGuards):
+        self.name = name
+        self.amountOfLifeGuards = amountOfLifeGuards
+        self.timeShifts = {"start1" : 7,
+                        "end1" : 15,
+                        "start2" : 15,
+                        "end2" : 22}
+        self.schifts = (list(range(1001, 1008))) * 2     #1032
+        for c in self.schifts:
+            i = self.schifts.index(c)
+            if i < len(self.schifts) * 0.5:
+                self.schifts[i] = c + 100       #str(c) +  "M"
+            else:
+                self.schifts[i] = c + 200       #str(c) +  "E"        
+        a = 1
+        schiftsAdded = []
+        while a < self.amountOfLifeGuards:            
+            for d in self.schifts:
+                e = d + (1000 * a)
+                schiftsAdded.append(e)
+            a += 1
+        for f in schiftsAdded:
+            self.schifts.append(f)        
+        fh = open(script_dir + "\\data\\" + str(self.name) + ".dat", "wb")
+        pickle.dump(self.schifts, fh)
+        fh.close
+        self.schedule = {}
+        self.addPoolToPoolList()
+
+    def addPoolToPoolList(self):
+        PoolList.append(self)
+
+    def __del__(self):
+        PoolList.remove(self)
+
+    def showPoolInfo(self):
+        return {"Nazwa" : self.name,
+                "Pierwsza zmiana" : "od godz. " + str(self.timeShifts["start1"]) + " do godz. " + str(self.timeShifts["end1"]),
+                "Druga zmiana" : "od godz. " + str(self.timeShifts["start2"]) + " do godz. " + str(self.timeShifts["end2"])}
+
+#Osowa = Pool("Osowa", 3)
+#print(Osowa.schifts)
---
+++ main.py
@@ -0,0 +1,115 @@
+from Classes import *
+import random
+
+Romanowski = LifeGuard("Grzegorz", "Romanowski", 910615, 20)
+Romanowska = LifeGuard("Patrycja", "Romanowska", 910219, 20)
+RomanowskaSara = LifeGuard("Sara", "Romanowska", 192729, 20)
+RomanowskaIga = LifeGuard("Iga", "Romanowska", 222516, 20)
+#del Romanowski
+#print(EmployeeList)
+#print(Romanowska.showLifeGuardInfo())
+
+Łapino = Pool("Łapino", 1)
+Osowa = Pool("Osowa", 2)
+Łapino.timeShifts["start1"] = 9
+Osowa.timeShifts["start1"] = 6
+#del Osowa
+#print(PoolList)
+#print(Łapino.showPoolInfo())
+#print(Osowa.showPoolInfo())
+
+Romanowski.setAvailability()
+Romanowska.setAvailability()
+RomanowskaSara.setAvailability()
+RomanowskaIga.setAvailability()
+Romanowski.preferredPool = Osowa.name
+Romanowska.preferredPool = Osowa.name
+RomanowskaSara.preferredPool = Łapino.name
+RomanowskaIga.preferredPool = Łapino.name
+#print(Romanowska.availability)
+
+#print(Osowa.schifts)
+#print(Łapino.schifts)
+
+def willing3(sp, x):      #np. Osowa, 1, M
+    willingList = []
+    for LG in EmployeeList:
+        if LG.preferredPool == sp:
+            if LG.availability[x] == 3:
+                willingList.append(LG)
+    return willingList
+
+def willing(sp, x, y):      #np. Osowa, 1, M
+    willingList = []
+    for LG in EmployeeList:
+        if LG.preferredPool == sp:
+            if LG.availability[x] == y or LG.availability[x] == 3:
+                willingList.append(LG)            
+    return willingList
+
+def match():
+    for pool in PoolList:
+        schiftRemoved = []        
+        for schift in pool.schifts:            
+            xx = schift % 100
+            listOfWilling = willing3(pool.name, xx)
+            try:
+                winner = random.choice(listOfWilling)
+                if (((schift + 100)//100)%10) == 1 or (((schift + 100)//100)%10) == 2:
+                    schift2 = schift + 100
+                elif   (((schift - 100)//100)%10) == 1 or (((schift - 100)//100)%10) == 2:
+                    schift2 = schift - 100
+                if schift not in pool.schedule:
+                    pool.schedule[schift] = winner.name
+                if schift2 not in pool.schedule:
+                    pool.schedule[schift2] = winner.name                      
+                if schift not in winner.schedule:
+                    winner.schedule[schift] = pool.name
+                if schift2 not in winner.schedule:
+                    winner.schedule[schift2] = pool.name                       
+                winner.availability[xx] = 0                          
+                if schift not in schiftRemoved:
+                    schiftRemoved.append(schift)
+                if schift2 not in schiftRemoved:
+                    schiftRemoved.append(schift2)
+            except (IndexError):
+                None
+        for s in schiftRemoved:
+            pool.schifts.remove(s)
+
+    for pool in PoolList:
+        schiftRemoved = []        
+        for schift in pool.schifts:            
+            if (schift % 1000) < 199:
+                yy = 1
+            else:
+                yy = 2
+            xx = schift % 100
+            listOfWilling = willing(pool.name, xx, yy)
+            try:
+                winner = random.choice(listOfWilling)
+                pool.schedule[schift] = winner.name          
+                winner.schedule[schift] = pool.name            
+                winner.availability[xx] -= yy                
+                schiftRemoved.append(schift)
+            except (IndexError):
+                None
+        for s in schiftRemoved:
+            pool.schifts.remove(s)
+            
+
+match()
+print("!!! OSOWA !!!")
+print(Osowa.schedule)
+print("!!! ŁAPINO !!!")
+print(Łapino.schedule)
+print("!!! Romanowski !!!")
+print(Romanowski.schedule)
+print("!!! Romanowska !!!")
+print(Romanowska.schedule)
+print("!!! Sara !!!")
+print(RomanowskaSara.schedule)
+print("!!! Iga !!!")
+print(RomanowskaIga.schedule)
+
+print("KONIEC")
