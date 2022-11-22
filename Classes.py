9c09ce1686daa0ab759d431f606eeb2fff3c4a6f
Grzegorz Romanowski <118511601+GrzegorzRomanowski@users.noreply.github.com>
2022-11-23 00:36:18 +0100

    Update Classes.py

--- Classes.py
+++ Classes.py
@@ -16,10 +16,10 @@ class LifeGuard:
         self.addLifeGuardToEmployeeList()
 
     def addLifeGuardToEmployeeList(self):
-        EmployeeList.append(self)       #.PESEL
+        EmployeeList.append(self)
 
     def __del__(self):
-        EmployeeList.remove(self)       #.PESEL
+        EmployeeList.remove(self)
 
     def showLifeGuardInfo(self):
         return {"Imię" : self.name,
@@ -30,7 +30,7 @@ class LifeGuard:
 
     def setAvailability(self):
         v = 1
-        while v <= 7:      #31
+        while v <= 31:
             x = input(str(self.name) + "- Podaj dyspozycyjność w dniu " + str(v) + ". \n0-wolne, 1-pierwsza zmiana, 2-druga zmiana, 3-cały dzień:  ")
             try:
                 if int(x) in [0, 1, 2, 3]:
@@ -53,24 +53,24 @@ class Pool:
                         "end1" : 15,
                         "start2" : 15,
                         "end2" : 22}
-        self.schifts = (list(range(1001, 1008))) * 2     #1032
-        for c in self.schifts:
-            i = self.schifts.index(c)
-            if i < len(self.schifts) * 0.5:
-                self.schifts[i] = c + 100       #str(c) +  "M"
+        self.shifts = (list(range(1001, 1032))) * 2
+        for c in self.shifts:
+            i = self.shifts.index(c)
+            if i < len(self.shifts) * 0.5:
+                self.shifts[i] = c + 100
             else:
-                self.schifts[i] = c + 200       #str(c) +  "E"        
+                self.shifts[i] = c + 200     
         a = 1
-        schiftsAdded = []
+        shiftsAdded = []
         while a < self.amountOfLifeGuards:            
-            for d in self.schifts:
+            for d in self.shifts:
                 e = d + (1000 * a)
-                schiftsAdded.append(e)
+                shiftsAdded.append(e)
             a += 1
-        for f in schiftsAdded:
-            self.schifts.append(f)        
+        for f in shiftsAdded:
+            self.shifts.append(f)        
         fh = open(script_dir + "\\data\\" + str(self.name) + ".dat", "wb")
-        pickle.dump(self.schifts, fh)
+        pickle.dump(self.shifts, fh)
         fh.close
         self.schedule = {}
         self.addPoolToPoolList()
@@ -85,6 +85,4 @@ class Pool:
         return {"Nazwa" : self.name,
                 "Pierwsza zmiana" : "od godz. " + str(self.timeShifts["start1"]) + " do godz. " + str(self.timeShifts["end1"]),
                 "Druga zmiana" : "od godz. " + str(self.timeShifts["start2"]) + " do godz. " + str(self.timeShifts["end2"])}
-
-#Osowa = Pool("Osowa", 3)
-#print(Osowa.schifts)
+    
